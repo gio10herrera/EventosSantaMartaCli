e9157a21b6243322e5f0c946b6680a6c4ca895d9
unknown <gio2020herrera@gmail.com>
2024-07-01 00:55:11 -0500

    lista de eventos terminada conectada a la data

--- components/CompListaEventos.tsx
+++ components/CompListaEventos.tsx
@@ -1,9 +1,26 @@
-import { Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+import { EVENTS_LIST } from "../data/data";
+import CompEventoItem from "./CompEventoItem";
 
 export default function CompListaEventos(){
     return (
-        <View>
-            <Text>Lista de eventos</Text>
+        <View style = {styles.container}>
+            <FlatList
+                data={EVENTS_LIST}
+                keyExtractor={item => item.id}
+                renderItem={({item}) => (
+                    <CompEventoItem evento = {item}/>
+                )}
+            >
+            </FlatList>
         </View>
+       
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#437070" 
+    }
+})
